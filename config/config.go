831935d5db45e530f3ee45ae71488f304b7080af
FenghuiLee <fenghuilee@gmail.com>
2025-03-16 20:11:38 +0800

    fix: 将废弃的 io/ioutil 替换为 os 包

--- config/config.go
+++ config/config.go
@@ -1,7 +1,7 @@
 package config
 
 import (
-	"io/ioutil"
+	"os"
 
 	"gopkg.in/yaml.v2"
 )
@@ -23,7 +23,7 @@ type Project struct {
 }
 
 func LoadConfig(filename string) (*Config, error) {
-	data, err := ioutil.ReadFile(filename)
+	data, err := os.ReadFile(filename)
 	if err != nil {
 		return nil, err
 	}
